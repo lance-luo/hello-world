ebfc534615c5f1c9eae4c4573cf16d3c68ac6c27
lance-luo <luochangan@gmail.com>
2017-07-11 13:32:38 +1000

    Update jenkins_watchdog.py

--- jenkins_watchdog.py
+++ jenkins_watchdog.py
@@ -8,6 +8,8 @@ def main(argv):
     # My code here
 	status = os.popen('service jenkins status|grep "Active"').readline()
 	print status
+	if status.find('dead') =! -1:
+	    print 'Jenkins is dead, needs to be restarted...'
 
 if __name__ == "__main__":
     main(sys.argv)
