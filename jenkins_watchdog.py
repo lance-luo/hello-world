0db3ddb5fc23c96377c79a9fdd002e6111133c78
lance-luo <luochangan@gmail.com>
2017-07-11 13:23:02 +1000

    Add files via upload

---
+++ jenkins_watchdog.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python
+"""This module provides functions for authenticating users."""
+
+import os
+import sys
+
+def main(argv):
+    # My code here
+	status = os.popen('command').readline())
+	print status
+
+if __name__ == "__main__":
+    main(sys.argv)
